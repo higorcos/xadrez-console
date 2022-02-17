abe6d0c4383281352f97b9e23691ff2d4b840e55
higorcos <higorcosta2001@outlook.com>
2022-02-17 20:29:41 -0300

    Class posição

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console{
+    class Program{
+        static void Main(){
+            
+            Posicao A = new Posicao(2,3);
+
+            System.Console.WriteLine(A);
+        }
+    }
+}
